52a54c445545cc212896c91512c0833a6e08b927
Dmitry <ns.global@mail.ru>
2018-04-17 14:25:30 +0300

    init

---
+++ app/Http/Controllers/api/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dima
+ * Date: 17.04.18
+ * Time: 14:00
+ */
+
+namespace App\Http\Controllers\api;
+
+
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        //
+    }
+}
