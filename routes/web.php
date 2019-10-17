c0f14f8a757806748db4dcebbebb71ab12b3d231
dmitry-neposidyaka <dmitry.neposidyaka@uadevelopers.com>
2019-10-17 15:44:45 +0300

    reorder routes

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,16 @@
 <?php
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
-use GuzzleHttp\Client;
 
+$router->group(['namespace' => 'api', 'middleware' => ['cors']], function () use($router){
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+    $router->post('/auth', 'AuthController@auth');
+    $router->post('/auth-soc', 'AuthController@authSoc');
+    $router->post('/registration', 'AuthController@registration');
+/*    $router->get('/auth', function (){
+        return view('auth', ['result' => null]);
+    });
+    $router->get('/registration', function (){
+        return view('registration', ['result' => null]);
+    });*/
 });
 
 $router->group(['namespace' => 'api', 'prefix' => 'api', 'middleware' => 'cors'], function () use($router){
@@ -32,15 +29,3 @@ $router->group(['namespace' => 'api', 'prefix' => 'api', 'middleware' => 'cors']
     $router->get('/password/email', 'CustomerController@email');
 });
 
-$router->group(['namespace' => 'api', 'middleware' => ['cors']], function () use($router){
-
-    $router->post('/auth', 'AuthController@auth');
-    $router->post('/auth-soc', 'AuthController@authSoc');
-    $router->post('/registration', 'AuthController@registration');
-    $router->get('/auth', function (){
-        return view('auth', ['result' => null]);
-    });
-    $router->get('/registration', function (){
-        return view('registration', ['result' => null]);
-    });
-});
